df83b461dfa859a719f2e49753493d98f87d092b
Jan Lahoda <jlahoda@netbeans.org>
2025-03-14 21:47:29 +0100

    WIP: test

--- ide/lsp.client/test/unit/src/org/netbeans/modules/lsp/client/bindings/CompletionProviderImplTest.java
+++ ide/lsp.client/test/unit/src/org/netbeans/modules/lsp/client/bindings/CompletionProviderImplTest.java
@@ -18,7 +18,49 @@
  */
 package org.netbeans.modules.lsp.client.bindings;
 
+import com.google.common.base.Function;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+import javax.swing.JEditorPane;
+import javax.swing.text.JTextComponent;
+import java.util.concurrent.CompletableFuture;
+import javax.swing.text.StyledDocument;
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.CompletionParams;
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.InsertTextFormat;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
+import org.netbeans.junit.MockServices;
 import org.netbeans.junit.NbTestCase;
+import org.netbeans.modules.editor.completion.CompletionImpl;
+import org.netbeans.modules.editor.completion.CompletionResultSetImpl;
+import org.netbeans.modules.lsp.client.TestUtils.MimeDataProviderImpl;
+import org.netbeans.modules.lsp.client.TestUtils.MockLSP;
+import org.netbeans.modules.lsp.client.TestUtils.MockMimeResolver;
+import org.netbeans.spi.editor.completion.CompletionProvider;
+import org.netbeans.spi.editor.completion.CompletionTask;
+import org.openide.cookies.EditorCookie;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Exceptions;
 
 public class CompletionProviderImplTest extends NbTestCase {
 
@@ -27,20 +69,225 @@ public class CompletionProviderImplTest extends NbTestCase {
     }
 
     public void testConvertSnippet2CodeTemplate() {
-        doRunTest("println(${1:args});$0",
+        doRunConvertSnippetTest("println(${1:args});$0",
                   "println(${T1 default=\"args\"});${cursor}");
-        doRunTest("println(${1:args}, $1);$0",
+        doRunConvertSnippetTest("println(${1:args}, $1);$0",
                   "println(${T1 default=\"args\"}, ${T1});${cursor}");
         //choices are not supported currently:
-        doRunTest("println(${1|one,two,three|});$0",
+        doRunConvertSnippetTest("println(${1|one,two,three|});$0",
                   "println(${T1});${cursor}");
         //variables are not supported currently:
-        doRunTest("println(${TM_SELECTED_TEXT:/upcase});$0",
+        doRunConvertSnippetTest("println(${TM_SELECTED_TEXT:/upcase});$0",
                   "println(${P0});${cursor}");
     }
 
-    private void doRunTest(String snippet, String template) {
+    private void doRunConvertSnippetTest(String snippet, String template) {
         String converted = CompletionProviderImpl.convertSnippet2CodeTemplate(snippet);
         assertEquals(template, converted);
     }
+
+    //TODO: also test that CC is not called when CC is not supported in the configuration!
+    public void testCommitWithResolveInsertPlainTextAndAdditionalEdits() throws IOException {
+        runCompletionDefaultActionTest(
+                """
+                foo.ins|
+                """,
+                new TestTextDocumentService() {
+                    @Override
+                    public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams position) {
+                        return CompletableFuture.completedFuture(Either.forLeft(List.of(
+                            new CompletionItem("to-be-resolved")
+                        )));
+                    }
+                    @Override
+                    public CompletableFuture<CompletionItem> resolveCompletionItem(CompletionItem unresolved) {
+                        assertEquals("to-be-resolved", unresolved.getLabel());
+
+                        CompletionItem resolved = new CompletionItem(unresolved.getLabel());
+
+                        resolved.setInsertText("inserted");
+                        resolved.setInsertTextFormat(InsertTextFormat.PlainText);
+                        resolved.setAdditionalTextEdits(List.of(new TextEdit(new Range(new Position(0, 0), new Position(0, 1)), "new-")));
+
+                        return CompletableFuture.completedFuture(resolved);
+                    }
+                },
+                """
+                new-oo.inserted
+                """);
+    }
+
+    public void testCommitWithResolveInsertSnippetAndAdditionalEdits() throws IOException {
+        runCompletionDefaultActionTest(
+                """
+                foo.ins|
+                """,
+                new TestTextDocumentService() {
+                    @Override
+                    public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams position) {
+                        return CompletableFuture.completedFuture(Either.forLeft(List.of(
+                            new CompletionItem("to-be-resolved")
+                        )));
+                    }
+                    @Override
+                    public CompletableFuture<CompletionItem> resolveCompletionItem(CompletionItem unresolved) {
+                        assertEquals("to-be-resolved", unresolved.getLabel());
+
+                        CompletionItem resolved = new CompletionItem(unresolved.getLabel());
+
+                        resolved.setInsertText("inserted(${1:args});$0");
+                        resolved.setInsertTextFormat(InsertTextFormat.Snippet);
+                        resolved.setAdditionalTextEdits(List.of(new TextEdit(new Range(new Position(0, 0), new Position(0, 1)), "new-")));
+
+                        return CompletableFuture.completedFuture(resolved);
+                    }
+                },
+                """
+                new-oo.inserted
+                """);
+    }
+
+    public void testCommitWithResolveMainEditAndAdditionalEdits() throws IOException {
+        runCompletionDefaultActionTest(
+                """
+                foo.ins|
+                """,
+                new TestTextDocumentService() {
+                    @Override
+                    public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams position) {
+                        return CompletableFuture.completedFuture(Either.forLeft(List.of(
+                            new CompletionItem("to-be-resolved")
+                        )));
+                    }
+                    @Override
+                    public CompletableFuture<CompletionItem> resolveCompletionItem(CompletionItem unresolved) {
+                        assertEquals("to-be-resolved", unresolved.getLabel());
+
+                        CompletionItem resolved = new CompletionItem(unresolved.getLabel());
+
+                        resolved.setTextEdit(Either.forLeft(new TextEdit(new Range(new Position(0, 4), new Position(0, 7)), "inserted")));
+                        resolved.setAdditionalTextEdits(List.of(new TextEdit(new Range(new Position(0, 0), new Position(0, 1)), "new-")));
+
+                        return CompletableFuture.completedFuture(resolved);
+                    }
+                },
+                """
+                new-oo.inserted
+                """);
+    }
+
+    private void runCompletionDefaultActionTest(String testCodeWithCaret,
+                                                TextDocumentService documentService,
+                                                String expectedOutput) throws IOException {
+        MockLSP.createServer = () -> new TestLanguageServer(documentService);
+        MockServices.setServices(MimeDataProviderImpl.class, MockMimeResolver.class);
+
+        FileObject folder = FileUtil.createMemoryFileSystem().getRoot().createFolder("myfolder");
+        FileObject testFile = folder.createData("data1.mock-txt");
+
+        int caret = testCodeWithCaret.indexOf('|');
+        String testCode = testCodeWithCaret.substring(0, caret) + testCodeWithCaret.substring(caret + 1);
+
+        try (OutputStream out = testFile.getOutputStream();
+             Writer w = new OutputStreamWriter(out)) {
+            w.write(testCode);
+        }
+
+        EditorCookie ec = testFile.getLookup().lookup(EditorCookie.class);
+        StyledDocument doc = ec.openDocument();
+        JTextComponent c = new JEditorPane();
+        c.setDocument(doc);
+        c.setCaretPosition(4);
+        CompletionTask task = new CompletionProviderImpl().createTask(CompletionProvider.COMPLETION_QUERY_TYPE, c);
+        CompletionResultSetImpl resultImpl = CompletionImpl.get().createTestResultSet(task, CompletionProvider.COMPLETION_QUERY_TYPE);
+        task.query(resultImpl.getResultSet());
+        while (!resultImpl.isFinished()) {
+            try {
+                Thread.sleep(1); //XXX
+            } catch (InterruptedException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        List<? extends org.netbeans.spi.editor.completion.CompletionItem> items = resultImpl.getItems();
+        assertEquals(1, items.size());
+        items.get(0).defaultAction(c);
+        assertEquals(expectedOutput, c.getText());
+    }
+
+    @Override
+    protected boolean runInEQ() {
+        return true;
+    }
+
+    private static final class TestLanguageServer implements LanguageServer {
+
+        private final TextDocumentService textDocumentService;
+        private final WorkspaceService workspaceService = new TestWorkspaceService();
+        private final Function<InitializeResult, InitializeResult> adjustInitializeResult;
+
+        public TestLanguageServer(TextDocumentService textDocumentService) {
+            this(textDocumentService, init -> init);
+        }
+
+        public TestLanguageServer(TextDocumentService textDocumentService,
+                                  Function<InitializeResult, InitializeResult> adjustInitializeResult) {
+            this.textDocumentService = textDocumentService;
+            this.adjustInitializeResult = adjustInitializeResult;
+        }
+
+        @Override
+        public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
+            InitializeResult result = new InitializeResult();
+            ServerCapabilities capa = new ServerCapabilities();
+            result.setCapabilities(capa);
+            return CompletableFuture.completedFuture(adjustInitializeResult.apply(result));
+        }
+
+        @Override
+        public CompletableFuture<Object> shutdown() {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public void exit() {
+        }
+
+        @Override
+        public TextDocumentService getTextDocumentService() {
+            return textDocumentService;
+        }
+
+        @Override
+        public WorkspaceService getWorkspaceService() {
+            return workspaceService;
+        }
+
+    }
+
+    private static class TestTextDocumentService implements TextDocumentService {
+
+        @Override
+        public void didOpen(DidOpenTextDocumentParams params) {}
+
+        @Override
+        public void didChange(DidChangeTextDocumentParams params) {}
+
+        @Override
+        public void didClose(DidCloseTextDocumentParams params) {}
+
+        @Override
+        public void didSave(DidSaveTextDocumentParams params) {}
+
+    }
+
+    private static class TestWorkspaceService implements WorkspaceService {
+
+        @Override
+        public void didChangeConfiguration(DidChangeConfigurationParams params) {}
+
+        @Override
+        public void didChangeWatchedFiles(DidChangeWatchedFilesParams params) {}
+
+    }
+
 }
