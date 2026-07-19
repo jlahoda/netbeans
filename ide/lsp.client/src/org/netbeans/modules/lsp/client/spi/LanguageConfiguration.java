e18c0b2bef2674957f8eae906935d08f1df2ffcc
Jan Lahoda <lahoda@gmail.com>
2026-07-19 12:50:46 +0200

    Adding LanguageConfiguration.

---
+++ ide/lsp.client/src/org/netbeans/modules/lsp/client/spi/LanguageConfiguration.java
@@ -0,0 +1,167 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.lsp.client.spi;
+
+import org.netbeans.api.annotations.common.NonNull;
+import org.netbeans.api.annotations.common.NullAllowed;
+
+//based on the VS Code API's "LanguageConfiguration" (MIT)
+public final class LanguageConfiguration {
+    public final @NullAllowed CommentRule comments;
+    public final @NullAllowed CharacterPair[] brackets;
+    public final @NullAllowed RegExp wordPattern;
+    public final @NullAllowed IndentationRule indentationRules;
+    public final @NullAllowed OnEnterRule[] onEnterRules;
+    public final @NullAllowed AutoClosingPair[] autoClosingPairs;
+
+    private LanguageConfiguration(CommentRule comments, CharacterPair[] brackets, RegExp wordPattern, IndentationRule indentationRules, OnEnterRule[] onEnterRules, AutoClosingPair[] autoClosingPairs) {
+        this.comments = comments;
+        this.brackets = brackets;
+        this.wordPattern = wordPattern;
+        this.indentationRules = indentationRules;
+        this.onEnterRules = onEnterRules;
+        this.autoClosingPairs = autoClosingPairs;
+    }
+    
+    public static LanguageConfiguration from(CommentRule comments, CharacterPair[] brackets, RegExp wordPattern, IndentationRule indentationRules, OnEnterRule[] onEnterRules, AutoClosingPair[] autoClosingPairs) {
+        return new LanguageConfiguration(comments, brackets, wordPattern, indentationRules, onEnterRules, autoClosingPairs);
+    }
+
+    public static final class RegExp {
+        public final @NonNull String regExp;
+
+        public RegExp(String regExp) {
+            this.regExp = regExp;
+        }
+
+        public static RegExp from(String regExp) {
+            return new RegExp(regExp);
+        }
+        
+    }
+    public static final class CharacterPair {
+        public final String first;
+        public final String second;
+
+        private CharacterPair(String first, String second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public static CharacterPair from(String first, String second) {
+            return new CharacterPair(first, second);
+        }
+    }
+
+    public static final class CommentRule {
+        public final @NullAllowed String lineComment;
+        public final @NullAllowed CharacterPair blockComment;
+
+        private CommentRule(String lineComment, CharacterPair blockComment) {
+            this.lineComment = lineComment;
+            this.blockComment = blockComment;
+        }
+
+        public static CommentRule from(String lineComment, CharacterPair blockComment) {
+            return new CommentRule(lineComment, blockComment);
+        }
+    }
+
+    public static final class IndentationRule {
+        public @NullAllowed RegExp decreaseIndentPattern;
+        public @NullAllowed RegExp increaseIndentPattern;
+        public @NullAllowed RegExp indentNextLinePattern;
+        public @NullAllowed RegExp unIndentedLinePattern;
+
+        private IndentationRule(RegExp decreaseIndentPattern, RegExp increaseIndentPattern, RegExp indentNextLinePattern, RegExp unIndentedLinePattern) {
+            this.decreaseIndentPattern = decreaseIndentPattern;
+            this.increaseIndentPattern = increaseIndentPattern;
+            this.indentNextLinePattern = indentNextLinePattern;
+            this.unIndentedLinePattern = unIndentedLinePattern;
+        }
+        
+        public static IndentationRule from(RegExp decreaseIndentPattern, RegExp increaseIndentPattern, RegExp indentNextLinePattern, RegExp unIndentedLinePattern) {
+            return new IndentationRule(decreaseIndentPattern, increaseIndentPattern, indentNextLinePattern, unIndentedLinePattern);
+        }
+    }
+    
+    public static enum IndentAction {
+        None,
+        Indent,
+        IndentOutdent,
+        Outdent
+    }
+
+    public static final class EnterAction {
+        public final @NonNull IndentAction indentAction;
+        public final @NullAllowed String appendText;
+        public final @NullAllowed Integer removeText;
+
+        private EnterAction(IndentAction indentAction, String appendText, Integer removeText) {
+            this.indentAction = indentAction;
+            this.appendText = appendText;
+            this.removeText = removeText;
+        }
+
+        public static EnterAction from(IndentAction indentAction, String appendText, Integer removeText) {
+            return new EnterAction(indentAction, appendText, removeText);
+        }
+    }
+
+    public static final class OnEnterRule {
+        public final @NonNull RegExp beforeText;
+        public final @NullAllowed RegExp afterText;
+        public final @NullAllowed RegExp previousLineText;
+        public final @NonNull EnterAction action;
+
+        private OnEnterRule(RegExp beforeText, RegExp afterText, RegExp previousLineText, EnterAction action) {
+            this.beforeText = beforeText;
+            this.afterText = afterText;
+            this.previousLineText = previousLineText;
+            this.action = action;
+        }
+        
+        public static OnEnterRule from(RegExp beforeText, RegExp afterText, RegExp previousLineText, EnterAction action) {
+            return new OnEnterRule(beforeText, afterText, previousLineText, action);
+        }
+    }
+
+    public static enum SyntaxTokenType {
+        Other,
+        Comment,
+        String,
+        RegEx
+    }
+    
+    public static final class AutoClosingPair {
+        public @NonNull String open;
+        public @NonNull String close;
+        public @NullAllowed SyntaxTokenType[] notIn;
+
+        private AutoClosingPair(String open, String close, SyntaxTokenType[] notIn) {
+            this.open = open;
+            this.close = close;
+            this.notIn = notIn;
+        }
+        
+        public static AutoClosingPair from(String open, String close, SyntaxTokenType[] notIn) {
+            return new AutoClosingPair(open, close, notIn);
+        }
+    }
+}
